1636f153a990ea09b97eca1bcf18b28090dbab5c
Gorez <gorez.94@mail.ru>
2023-10-08 18:11:23 +0600

    дома

---
+++ tusk04/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Внутри класса Answer напишите метод FindeMax,
+который принимает на вход три числа и выдает макс из них
+*/
+System.Console.WriteLine("Введите три числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+System.Console.WriteLine("Максимальное число: " + max);
---
+++ tusk06/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Внутри класса Answer напишите метод CheckIfEven
+который на вход принимает число number и выводит, является ли число четным
+(делится ли оно на 2 без остатка)
+*/
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+}
+else
+{
+    System.Console.WriteLine("Число нечетное");
+}
