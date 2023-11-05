938cbd1cc34b0a9a2fe9eaeac06e0871f5ede95c
Gorez <gorez.94@mail.ru>
2023-11-05 18:04:04 +0600

    47-41-43-50

---
+++ tusk47_random_mass_mnx/Program.cs
@@ -0,0 +1,46 @@
+﻿/*47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+*/
+
+
+int TakeEnteredNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+double[,] GetArray(int row, int column)
+{
+    double[,] array = new double[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10) + new Random().NextDouble();
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+        System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int userArrayRow = TakeEnteredNumber("Введите количестов строк:");
+int userArrayColumn = TakeEnteredNumber("Введите количестов столбцов:");
+int userArrayStart = TakeEnteredNumber("Введите начало диапозона:");
+int userArrayEnd = TakeEnteredNumber("Введите конец диапозона:");
+double[,] user2DArray = GetArray(userArrayRow, userArrayColumn);
+
+PrintArray(user2DArray);
---
+++ tusk50_poziciya/Program.cs
@@ -0,0 +1,74 @@
+﻿/*50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+
+int[,] GetArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+        System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void PoiskElementa(int[,] arr, int rows,int colomn)
+{
+    int iskomoe = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(rows == i && colomn == j)
+            {
+                iskomoe = arr[i, j];
+                break;
+            }
+        }
+        
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"{rows} и {colomn} -> {iskomoe}");
+}
+
+int rowsMass = 5;
+int colomnMass = 5;
+
+int[,] arrayPoisk = GetArray(rowsMass, colomnMass);
+PrintArray(arrayPoisk);
+System.Console.WriteLine();
+System.Console.WriteLine("Введите строку: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите столбец: ");
+int colomn = Convert.ToInt32(Console.ReadLine());
+if (rows >= rowsMass || colomn >= colomnMass)
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine($"{rows} и {colomn} -> Нет числа, адресс не верный");
+}
+else
+{
+    PoiskElementa(arrayPoisk, rows, colomn);
+}
