b89f464854ed6873940b1813243c652ea30c28e3
Gorez <gorez.94@mail.ru>
2023-11-21 18:24:52 +0600

    64-66-68

--- tusk55/Program.cs
+++ tusk55/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
+﻿
 int Prompt(string massage)
 {
     System.Console.Write(massage);
---
+++ tusk66_natur_MN_promejutok/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+void Summ(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма = {summ} ");
+        return;
+    }
+    Summ(m, n - 1, summ);
+}
+ System.Console.WriteLine("Введите число1: ");
+int m = Convert.ToInt32(Console.ReadLine());
+ System.Console.WriteLine("Введите число2: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+Summ(m, n, temp = 0);
